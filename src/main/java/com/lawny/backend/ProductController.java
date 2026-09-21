48b377a7b491f57757231e7b12003ece1ab1c277
Seth Fairburn <seth.fairburn@g.austincc.edu>
2026-09-21 10:25:30 -0500

    Completed Product CRUD operations

--- src/main/java/com/lawny/backend/ProductController.java
+++ src/main/java/com/lawny/backend/ProductController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/api/products")
@@ -22,9 +25,26 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(
+            @PathVariable("id") Long id,
+            @RequestBody Product updatedProduct) {
+
+        return productService.updateProduct(id, updatedProduct);
+    }
+
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         return productService.createProduct(product);
     }
 
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProduct(id);
+    }
 }
--- src/main/java/com/lawny/backend/ProductService.java
+++ src/main/java/com/lawny/backend/ProductService.java
@@ -15,8 +15,32 @@ public class ProductService {
         return productRepository.findAll();
     }
 
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
     public Product createProduct(Product product) {
         return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+
+        Product existingProduct = productRepository.findById(id).orElse(null);
+
+        if (existingProduct == null) {
+            return null;
+        }
+
+        existingProduct.setName(updatedProduct.getName());
+        existingProduct.setBrand(updatedProduct.getBrand());
+        existingProduct.setCategory(updatedProduct.getCategory());
+        existingProduct.setNotes(updatedProduct.getNotes());
+
+        return productRepository.save(existingProduct);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
     }
 
 }
