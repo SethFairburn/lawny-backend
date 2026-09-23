e79635ad7bf238d56830fdc756b56c3254220a02
Seth Fairburn <seth.fairburn@g.austincc.edu>
2026-09-23 17:23:32 -0500

    Connected front end to Watering API

--- src/main/java/com/lawny/backend/watering/WateringController.java
+++ src/main/java/com/lawny/backend/watering/WateringController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/watering")
 public class WateringController {
